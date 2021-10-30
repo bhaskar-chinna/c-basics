34890e389b9f6c2947a571372b0a9964c5d657ae
bhaskar-chinna <92973364+bhaskar-chinna@users.noreply.github.com>
2021-10-29 23:27:51 -0700

    Add files via upload

---
+++ bhaskar calculations.c
@@ -0,0 +1,17 @@
+ #include<stdio.h>
+ void main ()
+ {
+ 	//variables
+ 	 int num1, num2, sum;
+ 	 
+ 	//initialization
+	 num1= 123;
+	 num2= 456; 
+	 
+	 //calculations
+	 sum = num1+num2;
+	 
+	 //output
+	 printf("sum of %d and %d is %d",num1,num2,sum); 
+ 	
+ }

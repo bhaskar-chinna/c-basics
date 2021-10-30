56bf62898f264b3f472f0750843f67870e273229
bhaskar-chinna <92973364+bhaskar-chinna@users.noreply.github.com>
2021-10-29 23:50:05 -0700

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//variable
+	int num1,num2,sum;
+	
+	//intialization
+	num1=254;
+	num2=32155;
+	
+	//calculation
+	sum=num1+num2;
+	
+	//out put
+	printf("%d",sum);
+}
---
+++ variable 22.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	int num1=10,num2=20,sum=num1+num2;
+	
+	printf("sum of%d and %d is %d",num1,num2,sum);
+}

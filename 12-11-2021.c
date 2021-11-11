b3477668514dd4ca22655bf2e32a6c5cd1c49ee6
bhaskar-chinna <92973364+bhaskar-chinna@users.noreply.github.com>
2021-11-11 12:22:28 +0530

    Add files via upload

---
+++ 12-11-2021.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main ()
+{
+	int s , a , p ;
+	printf("enter side");  
+	scanf("%d",&s);
+	a=s*s;
+	p=4*s;
+	printf("area of the perimeter is %d\n",a);
+	printf("perimeter of the squre is %d",p);
+}
